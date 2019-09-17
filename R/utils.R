84fd3bfa15fef6e321d39e30e4dca43f301d83b9
pablogonzalezginestet <pablo.gonzalezginestet@mail.mcgill.ca>
2019-09-17 11:16:29 +0200

    added Algorithm 2

--- R/utils.R
+++ R/utils.R
@@ -243,3 +243,63 @@ ML_list_natively <- list(
   
 )
 
+############################# Algorithm 2 Ensemble IPCW Bagging ##############################################
+#' @description  
+#' @param folds Number of folds
+#' @param MLprocedures \link{MLprocedures}
+#' @param fmla formula object ex. "E ~ x1+x2"
+#' @param tuneparams a list of tune parameters for each machine learning procedure
+#' @param B number of bootstrap samples
+#' @param data a training data set
+#' @param A a number of machine learning algorithms in the library
+#' @return a list with the predictions of each machine learning algorithm and the average AUC across folds for each of them
+#' @rdname EnsBagg-internal
+
+
+crossval_function <- function(folds, MLprocedures, fmla, tuneparams , B=NULL, data,A) {
+  result <- vector("list", A)
+  AUC.train <- vector("list", A)
+  result_id <- vector("list")
+  test.id_temp=1:nrow(data)
+  for (k in 1:folds) {
+    if (k==folds){
+      test.id=test.id_temp
+    }else{
+      test.id=sample(test.id_temp,floor( nrow(data)/folds ), replace=FALSE)
+      test.id_temp <- setdiff(test.id_temp, test.id) # update the test id candidates
+    }
+    train.id <- setdiff(1:nrow(data), test.id)
+    train.set <- data.frame(data[train.id,])
+    test.set <- data.frame(data[test.id,])
+    n_test.set <- nrow(test.set)
+    
+    #boot
+    b <-boot(data=train.set, statistic=MLprocedures, R=B, fmla=formula,tune.params=tune.params,
+             testdata=test.set, weights = train.set$wts)
+    
+    d<- apply(b$t,1,function(x) split(x, rep(seq(A), each = n_test.set)))
+    D.all <- list()
+    d.all <- NULL
+    for(s in 1:A){
+      for(j in 1:B){
+        d.all <- rbind(d.all,unlist(d[[j]][s],use.names = FALSE)) #given a ml procedure i go over the bootstrap for that given ml procedure
+      }
+      D.all <- colMeans(d.all,na.rm=T)
+      result[[s]]<-c(result[[s]], D.all)
+      AUC.train[[s]] <- c(AUC.train[[s]], AUC_function(T=test.set$ttilde,delta=test.set$delta,marker=D.all,cause=1,wts=test.set$wts.nn,tao))
+      d.all <- NULL
+    }
+    result_id[[k]]<- test.set$id
+    
+    # update progress bar
+    print(k)
+  }
+  
+  matrix = do.call(cbind, result)
+  prediction=as.data.frame(cbind(cbind(unlist(result_id)),matrix))
+  prediction=prediction[order(prediction[,1]),]
+  names(prediction) = c("id",paste("Set", 1:(ncol(prediction)-1), sep=""))
+  AUC.train <- sapply(AUC.train, function(x) sum(x)/folds)
+  return(list(prediction,AUC.train))
+  
+}
