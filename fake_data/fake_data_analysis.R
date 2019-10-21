9c482652bc33dc5c97ced4a026f28444eaad1ae5
pablogonzalezginestet <pablo.gonzalezginestet@mail.mcgill.ca>
2019-10-21 12:35:09 +0200

    update

--- fake_data/fake_data_analysis.R
+++ fake_data/fake_data_analysis.R
@@ -5,19 +5,22 @@ rm(list = ls())
 library(dplyr)
 library(ensBagg)
 
+#  we load the fake data set
 load("fake_data_train.RData")
 load("fake_data_test.RData")
 
-
-head(train)
-head(test)
-
-xnam <- names(fake_data_train)[-(1:2)]
+# we subset the train fake data set because the random forest crashes  but one could run all the following 
+# lines using the 2335 observations of fake_data_train 
 
 fake_data_train <- fake_data_train[sample(nrow(fake_data_train),1000,replace = FALSE),]
 
+# we define the variables to be included in the analysis
+xnam <- names(fake_data_train)[-(1:2)]
+
+# we set the grid of the hyperparameters that we are going to use in the tuning step
 grid.hyperparam <- ensBagg::grid_parametersDataHIV(xnam,fake_data_train,tao=730)
 
+# we tune the hyperparameters using the grid 
 tuneparams_fakedata<- ensBagg::tune_params_ml(gam_param = grid.hyperparam$gam_param, 
                                                  lasso_param = grid.hyperparam$lasso_param,
                                                  randomforest_param = grid.hyperparam$randomforest_param,
@@ -30,7 +33,10 @@ tuneparams_fakedata<- ensBagg::tune_params_ml(gam_param = grid.hyperparam$gam_pa
                                                  tao=730,
                                                  data=fake_data_train )
 
-pred_fakedata<- ensBagg::ensBagg(train.data = train,test.data = test,xnam = xnam,tao = 730,weighting = "CoxBoost",folds = 5,tuneparams=tuneparams_fakedata)
+# we specify the algorithms
+ens.library <-ensBagg::ens.all.algorithms()
 
+# we predict the outcome of interest using the ensBagg with Cox PH weights using all algorithms available in the package
+res.ensBagg <- ensBagg::ensBagg(train.data = fake_data_train,test.data = fake_data_test,xnam=xnam,tao=730,weighting = "CoxPH",folds = 5,ens.library = ens.library )
 
 
