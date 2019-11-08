875001c88dbe5ca32ed7faa082bfcbbbc895a870
pablogonzalezginestet <pablo.gonzalezginestet@mail.mcgill.ca>
2019-11-08 18:00:55 +0100

    added figure function

--- simulations/simulations.R
+++ simulations/simulations.R
@@ -1,5 +1,6 @@
 
 # This script replicate the Simulation Study, section 5, from  Gonzalez Ginestet et al. (2019+). "Ensemble IPCW Bagging bagging: a case study in the HIV care registry"
+# # It reproduces the figures showing the AUC of each single method relative to the ensemble and the tables with average estimated AUCs that are found in the supplementary material.
 # The setting is the same as it is described in the paper:
 # two ways of computing the IPC weights: Cox PH and Cox Boost
 # 2 simulation studies and 4 scenarios in each simulation study
@@ -63,8 +64,59 @@ table1_paper<- function(res,J,scenarios){
   return(table1)
 }
 
-####     end of functions  #### 
 
+figure1_paper<- function(res,J,scenarios){
+  num_scen<- length(scenarios)
+  AUC_sim<- vector("list", num_scen)
+  data_boxplot<- vector("list", num_scen)
+  upper_panel <- matrix(NA,nrow = J,17)
+  for(j in 1:J){
+    upper_panel[j,] <- c(rep(1,9),rep(2,5),rep(3,3))
+  }
+  colnames(upper_panel) <- c(rep("IPCW Bagging",9),rep("Native Weight",5),rep("Survival",3))
+ 
+  for(s in 1:num_scen){
+    for(j in 1:J){
+      
+      AUC_sim[[s]] <- rbind(AUC_sim[[s]],c(res[[s]][[j]]$auc_ipcwBagg[,-10],res[[s]][[j]]$auc_native_weights,res[[s]][[j]]$auc_survival)/res[[s]][[j]]$auc_ipcwBagg[,10] )
+      
+    }
+    colnames(AUC_sim[[s]]) <- c("LR","GAM.3","GAM.4","LASSO","RF","SVM","BART","k-NN","NNet","LR","GAM.3","GAM.4",
+                                "LASSO",  "RF", "Cox" ,"CoxBoost" , "RF")
+    data_boxplot[[s]] <- melt(AUC_sim[[s]])
+    data_boxplot[[s]]$scenario <- 1
+    data_boxplot[[s]] <- cbind(data_boxplot[[s]], melt(upper_panel))
+    
+  }
+  
+  data_boxplot1<- rbind( data_boxplot[[1]],data_boxplot[[2]],data_boxplot[[3]],data_boxplot[[4]] )
+  data_boxplot1 <- data_boxplot1[,c(-1,-5,-7)]
+  colnames(data_boxplot1) <- c("ml","r.auc","scenario","method")
+  
+  p <- ggplot(data_boxplot1, aes(ml, r.auc,fill=factor(scenario)))
+  p <- p + geom_boxplot()  + labs(x="\n Machine Learning (ML) Algorithms",y=TeX("$AUC_{ML} /  AUC_{Ensemble} \n "),fill = "Scenario") +
+    facet_grid(~method,  scales = "free_x", space = "free_x") +
+    geom_hline(aes(yintercept = 1)) + scale_y_continuous(breaks=seq(0.5,1.2,.05)) +
+    theme_bw() +  theme(panel.grid.major = element_blank(),plot.margin = margin(10,0,10,0),
+                        axis.title.x = element_text( size=25),
+                        axis.title.y = element_text( size=25),
+                        axis.text.x = element_text( 
+                          size=20),
+                        axis.text.y = element_text( 
+                          size=20)) +
+    theme(strip.text.x = element_text(size = 25)) +
+    theme(legend.position = "none") +  scale_fill_grey()
+  
+  return(p)
+  
+}
+
+####     end of functions  #### 
+#libraries for the plot
+library(reshape2)
+library(ggplot2)
+library(tikzDevice)
+library(latex2exp)
 
 J <- 500# number of simulations
 xnam <- paste("X", 1:20, sep="") # names of the covariates 
@@ -76,11 +128,23 @@ ens.library <- ensBagg::ens.all.algorithms()
 res_simulation1=lapply(scenarios, function(s) simulation_all_scenarios(weighting = "CoxPH",d=1,s) )
 table1_sim1 <- table1_paper(res_simulation1,J,scenarios)
 row.names(table1_sim1)[1] <- "True"
+figure1_sim1 <-figure1_paper(res_simulation1,J,scenarios) 
 
 # Simulation 2
 res_simulation2=lapply(scenarios, function(s) simulation_all_scenarios(weighting = "CoxPH",d=2,s) )
 table1_sim2 <- table1_paper(res_simulation2,J,scenarios)
 row.names(table1_sim2)[1] <- "True"
+figure1_sim2 <-figure1_paper(res_simulation2,J,scenarios) 
+
+
+# AUCs of each algorithm relative to the AUC of the ensemble based on 500 simulated data sets
+#under the four scenarios (A being the darkest and D the whitest, alphabetically order)  using
+#all available covariates and a Cox-PH model for censoring for predicting the event of interest in the test data set.
+#The horizontal line denotes the ensemble
+
+figure1_sim1 #simulation 1
+figure1_sim2 #simulation 2
+
 
 # Table 1: Average Estimated AUCs across 500 data sets for Simulation 1 and 2 and their four scenarios (A, B, C and D) using
 # all available covariates and a Cox-PH model for censoring for predicting the event of interest in the test data set.
@@ -95,11 +159,22 @@ xtable::xtable(cbind(table1_sim1,table1_sim2),digits=c(0,rep(3,16)))
 res_simulation1=lapply(scenarios, function(s) simulation1_all_scenarios(weighting = "CoxBoost",d=1,s) )
 table1_sim1 <- table1_paper(res_simulation1,J,scenarios)
 row.names(table1_sim1)[1] <- "True"
+figure1_sim1 <-figure1_paper(res_simulation1,J,scenarios) 
 
 # Simulation 2
 res_simulation2=lapply(scenarios, function(s) simulation1_all_scenarios(weighting = "CoxBoost",d=2,s) )
 table1_sim2 <- table1_paper(res_simulation2,J,scenarios)
 row.names(table1_sim2)[1] <- "True"
+figure1_sim2 <-figure1_paper(res_simulation1,J,scenarios) 
+
+# AUCs of each algorithm relative to the AUC of the ensemble based on 500 simulated data sets
+#under the four scenarios (A being the darkest and D the whitest, alphabetically order)  using
+#all available covariates and a CoxBoost model for censoring for predicting the event of interest in the test data set.
+#The horizontal line denotes the ensemble
+
+figure1_sim1 #simulation 1
+figure1_sim2 #simulation 2
+
 
 # Table 2: Average Estimated AUCs across 500 data sets for Simulation 1 and 2 and their four scenarios (A, B, C and D) using
 # all available covariates and a CoxBoost model for censoring for predicting the event of interest in the test data set.
